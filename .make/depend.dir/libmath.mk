eb7393a9985f355d500411c98f16f77a106ad4a4
sparrow <sparrow@wsl.me>
2025-09-07 00:09:36 +0800

    Sun Sep  7 00:09:36 CST 2025

--- .make/depend.dir/libmath.mk
+++ .make/depend.dir/libmath.mk
@@ -1,12 +1,12 @@
 DEPEND_TARGETS += libmath.recursive
 ECHO_TARGETS += libmath.echo
 libmath.recursive:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/libraries/libfft/               >> /home/lidali/alpine/libraries/libmath/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/libraries/libfft/.make >> /home/lidali/alpine/libraries/libmath/.make/super
-	cd /home/lidali/alpine/libraries/libmath/ && make recursive && make install
-	-rm /home/lidali/alpine/libraries/libmath/.make/super
+	@echo SUPER_MAKE_DIR=/mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libfft/               >> /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libmath/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libfft/.make >> /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libmath/.make/super
+	cd /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libmath/ && make recursive && make install
+	-rm /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libmath/.make/super
 libmath.echo:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/libraries/libfft/               >> /home/lidali/alpine/libraries/libmath/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/libraries/libfft/.make >> /home/lidali/alpine/libraries/libmath/.make/super
-	cd /home/lidali/alpine/libraries/libmath/ && make echo
-	-rm /home/lidali/alpine/libraries/libmath/.make/super
+	@echo SUPER_MAKE_DIR=/mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libfft/               >> /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libmath/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libfft/.make >> /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libmath/.make/super
+	cd /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libmath/ && make echo
+	-rm /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libmath/.make/super
