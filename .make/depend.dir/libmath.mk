a22d70ed85b214e889057c7649220ea75da3304f
foxintango@allinone.llama <foxintango@allinone.llama>
2025-09-15 07:35:24 +0800

    Mon Sep 15 07:35:24 AM CST 2025

--- .make/depend.dir/libmath.mk
+++ .make/depend.dir/libmath.mk
@@ -1,12 +1,12 @@
 DEPEND_TARGETS += libmath.recursive
 ECHO_TARGETS += libmath.echo
 libmath.recursive:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/libraries/libfft/               >> /home/lidali/alpine/libraries/libmath/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/libraries/libfft/.make >> /home/lidali/alpine/libraries/libmath/.make/super
-	cd /home/lidali/alpine/libraries/libmath/ && make recursive && make install
-	-rm /home/lidali/alpine/libraries/libmath/.make/super
+	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/libraries/libfft/               >> /volumes/llama/home/alpine/libraries/libmath/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/libraries/libfft/.make >> /volumes/llama/home/alpine/libraries/libmath/.make/super
+	cd /volumes/llama/home/alpine/libraries/libmath/ && make recursive && make install
+	-rm /volumes/llama/home/alpine/libraries/libmath/.make/super
 libmath.echo:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/libraries/libfft/               >> /home/lidali/alpine/libraries/libmath/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/libraries/libfft/.make >> /home/lidali/alpine/libraries/libmath/.make/super
-	cd /home/lidali/alpine/libraries/libmath/ && make echo
-	-rm /home/lidali/alpine/libraries/libmath/.make/super
+	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/libraries/libfft/               >> /volumes/llama/home/alpine/libraries/libmath/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/libraries/libfft/.make >> /volumes/llama/home/alpine/libraries/libmath/.make/super
+	cd /volumes/llama/home/alpine/libraries/libmath/ && make echo
+	-rm /volumes/llama/home/alpine/libraries/libmath/.make/super
