8f89cee0d685a110841112bc30ed75fd04b5f37b
李诗军 <lidali@AllinoneBook.local>
2025-10-14 15:57:31 +0800

    2025年10月14日 星期二 15时57分31秒 CST

--- .make/depend.dir/libmath.mk
+++ .make/depend.dir/libmath.mk
@@ -1,12 +1,12 @@
 DEPEND_TARGETS += libmath.recursive
 ECHO_TARGETS += libmath.echo
 libmath.recursive:
-	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/libraries/libfft/               >> /volumes/llama/home/alpine/libraries/libmath/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/libraries/libfft/.make >> /volumes/llama/home/alpine/libraries/libmath/.make/super
-	cd /volumes/llama/home/alpine/libraries/libmath/ && make recursive && make install
-	-rm /volumes/llama/home/alpine/libraries/libmath/.make/super
+	@echo SUPER_MAKE_DIR=/Users/lidali/alpine/libraries/libfft/               >> /Users/lidali/alpine/libraries/libmath/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/Users/lidali/alpine/libraries/libfft/.make >> /Users/lidali/alpine/libraries/libmath/.make/super
+	cd /Users/lidali/alpine/libraries/libmath/ && /Applications/Xcode.app/Contents/Developer/usr/bin/make recursive && /Applications/Xcode.app/Contents/Developer/usr/bin/make install
+	-rm /Users/lidali/alpine/libraries/libmath/.make/super
 libmath.echo:
-	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/libraries/libfft/               >> /volumes/llama/home/alpine/libraries/libmath/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/libraries/libfft/.make >> /volumes/llama/home/alpine/libraries/libmath/.make/super
-	cd /volumes/llama/home/alpine/libraries/libmath/ && make echo
-	-rm /volumes/llama/home/alpine/libraries/libmath/.make/super
+	@echo SUPER_MAKE_DIR=/Users/lidali/alpine/libraries/libfft/               >> /Users/lidali/alpine/libraries/libmath/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/Users/lidali/alpine/libraries/libfft/.make >> /Users/lidali/alpine/libraries/libmath/.make/super
+	cd /Users/lidali/alpine/libraries/libmath/ && /Applications/Xcode.app/Contents/Developer/usr/bin/make echo
+	-rm /Users/lidali/alpine/libraries/libmath/.make/super
